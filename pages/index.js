0c434f527307558c2056b8e649f286d23177144d
unknown <w.catalans@gmail.com>
2021-03-17 21:14:09 +0100

    fix(form-contact): Add wsp message

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,26 @@ import useTranslation from 'next-translate/useTranslation'
 import  React, { useState } from 'react'
 export default function Home() {
 	const { t } = useTranslation('common')
-	
+	const [email, setEmail] = useState('')
+	const [name, setName] = useState('')
+	const [message, setMessage] = useState('')
 	function sendWspMessage($event) {
 		$event.preventDefault()
-		console.log(this.state)
-		// window.open(
-		// 	`https://api.whatsapp.com/send?phone=+393515398439&text=Sono ${formName} ${formMessage}. Il mio email ${formEmail}`,
-		// 	'_blank' // <- This is what makes it open in a new window.
-		// )
+		window.open(
+			`https://api.whatsapp.com/send?phone=+393515398439&text=Sono ${name} ${message}. Il mio email ${email}`,
+			'_blank'
+		)
 	}
 	function sendMailMessage($event) {
 		$event.preventDefault()
-		console.log(`click`);
+		try {
+			const a = window.location.href=`mailto:contatto@capcat.it?subject=${name}&body=${message}`
+			console.log({a})
+			window.open(`mailto:contatto@capcat.it?subject=${name}&body=${message}`, '_self')
+		} catch (error) {
+			console.log(error)
+		}
+		
 	}
 	function onClickServiceTab(e) {
 		console.log(e)
@@ -23,10 +31,32 @@ export default function Home() {
 		console.log(e)
 		activeTab = activeTab === 'active' ? '' : 'active'
 	}
+	function onChangeMessage($event) {
+		const val = $event.target.value
+		if(val && val.length)
+			setMessage(val)
+	}
+	function onChangeName($event) {
+		const val = $event.target.value
+		if(val && val.length)
+			setName(val)
+	}
+	function onChangeEmail($event) {
+		const val = $event.target.value
+		if(val && val.length)
+			setEmail(val)
+	}
+	function isFormValid() {
+		return Boolean(email &&
+					email.length > 3 &&
+					name &&
+					name.length > 3 &&
+					message &&
+					message.length > 3)
+	}
 	
   return (
     <div id="wrapper" className="fade-in">
-				{/* <!-- Intro --> */}
 					<div id="intro">
 						<h1>CAP<br />
 						CAT</h1>
@@ -61,9 +91,6 @@ export default function Home() {
 					<div id="main">
 							<article id="about-us" className="post featured">
 								<header className="major">
-									{/* <!-- <span className="date">April 25, 2017</span> --> */}
-									{/* <!-- <h2><a href="#">And this is a<br /> */}
-									{/* massive headline</a></h2> --> */}
 									<p>{t('about-us-resume')}<br/>
 									<br />
 									Cloud, Mobile, Web.</p>
@@ -74,12 +101,9 @@ export default function Home() {
 								</ul>
 							</article>
 
-						{/* <!-- Posts --> */}
 							<section className="posts" id="services">
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 24, 2017</span> */}
-										 {/* --> */}
 
 										<h2><a href="#">{t('services-cloud')} <br />
                     {t('services-cloud-2')}</a></h2>
@@ -92,7 +116,6 @@ export default function Home() {
 								</article>
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 22, 2017</span> --> */}
 										<h2><a href="#">{t('services-problem-solving')} <br />
                     {t('services-problem-solving-2')}</a></h2>
 									</header>
@@ -104,7 +127,6 @@ export default function Home() {
 								</article>
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 18, 2017</span> --> */}
 										<h2><a href="#">{t('services-development')}<br />
                     {t('services-development-2')}</a></h2>
 									</header>
@@ -116,7 +138,6 @@ export default function Home() {
 								</article>
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 14, 2017</span> --> */}
 										<h2><a href="#">Salesforce<br /></a></h2>
 									</header>
 									<a href="#" className="image fit"><img src="images/saleforce.jpg" alt="" /></a>
@@ -127,7 +148,6 @@ export default function Home() {
 								</article>
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 11, 2017</span> --> */}
 										<h2><a href="#">Mobile apps<br />
 										</a></h2>
 									</header>
@@ -139,7 +159,6 @@ export default function Home() {
 								</article>
 								<article>
 									<header className="service__header">
-										{/* <!-- <span className="date">April 7, 2017</span> --> */}
 										<h2><a href="#">Graphic Design<br /></a></h2>
 									</header>
 									<a href="#" className="image fit"><img src="images/graphic-design.jpg" alt="" /></a>
@@ -151,34 +170,36 @@ export default function Home() {
 							</section>
 					</div>
 
-				{/* <!-- Footer --> */}
 					<footer id="footer">
 						<section>
 							<form method="post" action="#">
 								<div className="fields">
 									<div className="field">
 										<label htmlFor="name">{t('form-name')}</label>
-										<input id="form__name" type="text" name="name" id="name" />
+										<input id="form__name" onChange={onChangeName} type="text" name="name" id="name" />
 									</div>
 									<div className="field">
-										<label htmlFor="email">Email</label>
-										<input id="form__email" type="text" name="email" id="email" />
+										<label htmlFor="email">Email </label>
+										<input id="form__email" onChange={onChangeEmail} type="email" name="email" id="email" />
 									</div>
 									<div className="field">
 										<label htmlFor="message">{t('form-message')}</label>
-										<textarea id="form__message" name="message" id="message" rows="3"></textarea>
+										<textarea id="form__message" onChange={onChangeMessage} name="message" id="message" rows="3"></textarea>
 									</div>
 								</div>
 								<ul className="actions">
 									<li>
 										<input type="submit" 
 											onClick={sendMailMessage}
-											value="Send Message" />
+											disabled={!isFormValid()}
+											value={t('form-submit')} />
 									</li>
 									<li>
-										<input id="form__submit-wsp" type="submit" 
+										<input id="form__submit-wsp"
+											disabled={!isFormValid()} 
+											type="submit" 
 											onClick={sendWspMessage}
-											value="Send Message Whatsapp" />
+											value={`${t('form-submit')} Whatsapp`} />
 									</li>
 								</ul>
 							</form>
@@ -196,10 +217,7 @@ export default function Home() {
 							<section>
 								<h3>Social</h3>
 								<ul className="icons alt">
-									<li><a href="#" className="icon brands alt fa-twitter"><span className="label">Twitter</span></a></li>
-									<li><a href="#" className="icon brands alt fa-facebook-f"><span className="label">Facebook</span></a></li>
-									<li><a href="#" className="icon brands alt fa-instagram"><span className="label">Instagram</span></a></li>
-									<li><a href="#" className="icon brands alt fa-github"><span className="label">GitHub</span></a></li>
+									<li><a href="https://github.com/capcatit/capcat.github.io" className="icon brands alt fa-github"><span className="label">GitHub</span></a></li>
 								</ul>
 							</section>
 						</section>
